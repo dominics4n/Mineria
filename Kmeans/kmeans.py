2437c816ee8f909ed0b9bd830bfcbe4adb67e11d
dominics4n <dominics4n@gmail.com>
2025-11-10 17:46:45 -0600

    kmeans beta

--- Kmeans/kmeans.py
+++ Kmeans/kmeans.py
@@ -1,20 +1,33 @@
 import numpy as np
-#import matplotlib.pyplot as plt
-#from sklearn.datasets import make_blobs
+import csv
+from sklearn.cluster import KMeans
 
-k = 3
+id = []
+kys = []
+no_clusters = 5
+Clusters = [[],[],[],[],[]]
 
-clusters = {}
+with open("../muestra4s.csv", 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvfile)
+    for row in csvreader:
+        id.append(row[0])
+        datos = [float(row[1]),float(row[2]), float(row[3]), float(row[4])]
+        kys.append(datos)
 
-for idx in range(k):
-    center = np.random.rand(1,4)
-    print(str(center))
-    points = []
-    cluster = {
-        'center' : center,
-        'points' : []
-    }
-    
-    clusters[idx] = cluster
-    
-clusters
+kmeans = KMeans(n_clusters=5, random_state=42).fit(kys)
+print (kmeans)
+print(kmeans.cluster_centers_)
+
+grupo = kmeans.predict(kys)
+
+aurora = 0
+for num in grupo:
+    datos = [id[aurora],kys[aurora][0], kys[aurora][1], kys[aurora][2], kys[aurora][3]]
+    Clusters[num].append(datos)
+
+for x in range(no_clusters):
+    if Clusters[x]:
+        with open('./Clusters/Cluster0'+str(x)+'.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(Clusters[x])
